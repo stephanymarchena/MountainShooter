0e2538f448a6f7d00bd8ab414fc9cb6538645964
Stephany <stephanymarchenna@gmail.com>
2025-02-28 23:16:01 -0300

    Creating Mediator Pattern. Enemies destruction.

--- code/Const.py
+++ code/Const.py
@@ -6,7 +6,22 @@ COLOR_YELLOW = (255, 255, 0)
 COLOR_WHITE = (255, 255, 255)
 
 # E
-EVENT_ENEMY = pygame.USEREVENT +1
+ENTITY_HEALTH = {
+    'Level1Bg0': 999,
+    'Level1Bg1': 999,
+    'Level1Bg2': 999,
+    'Level1Bg3': 999,
+    'Level1Bg4': 999,
+    'Level1Bg5': 999,
+    'Level1Bg6': 999,
+    'Player1': 300,
+    'Player2': 300,
+    'Enemy1': 50,
+    'Enemy2': 60,
+}
+
+EVENT_ENEMY = pygame.USEREVENT + 1
+
 ENTITY_SPEED = {
     'Level1Bg0': 0,
     'Level1Bg1': 1,
